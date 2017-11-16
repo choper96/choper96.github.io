97ff0cf5b2faffc43c07161c037f179c9f678b3f
choper96 <wwwegaaa@gmail.com>
2017-11-16 18:01:49 +0500

    1

--- js/common.js
+++ js/common.js
@@ -1,3 +1,36 @@
+$(document).ready(function() {
+
+	$("#form").submit(function() {
+		$.ajax({
+			type: "POST",
+			url: "../mail.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			alert("Подписка оформлена.");
+			$("#form").trigger("reset");
+		});
+		return false;
+	});
+	
+});
+
+$(document).ready(function() {
+
+	$("#form_eng").submit(function() {
+		$.ajax({
+			type: "POST",
+			url: "../mail.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			alert("You have just.");
+			$("#form").trigger("reset");
+		});
+		return false;
+	});
+	
+});
 
 
 $(function() {
